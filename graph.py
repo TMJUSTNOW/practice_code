4e262b6a81f216eba2f1a575b961ce8d6c02ff08
Polina Giralt <polina.giralt@gmail.com>
2013-11-15 18:25:10 -0500

    broken!

--- graph.py
+++ graph.py
@@ -31,46 +31,55 @@ graph_dict2[13] = [7, 14]
 graph_dict2[14] = [8, 13]
 
 
+def find_shortest_path(open_path_list):
+    shortest_path = open_path_list[0]
+    for path in open_path_list:
+        if len(path) < shortest_path:
+            shortest_path = path
+    return shortest_path
+
+
+# def add_neighbor_to_path(current_path, neighbors):
+
+
 def traverse_graph(graph, target, start):
     open_path_list = [[]]
     open_path_list[0] = [start]
     current_path = [start]
-    closed_node_list = []
+    visited_node_list = []
     found_path_list = []
     print("\n\n\n")
 
     while(len(open_path_list) != 0):
 
-        for path in open_path_list:
-            print("open_path_list %s" % open_path_list)
-            current_path = copy.deepcopy(path)
-            # print("curent_path %s" % current_path)
-            closed_node_list.append(current_path[-1])
-            # print("closed_node_list %s" % closed_node_list)
-            available = graph[current_path[-1]]
-            open_path_list.remove(current_path)
-            print("current_path %s" % current_path)
-            print("open_path_list2 %s" % open_path_list)
+        print("open_path_list %s" % open_path_list)
+        current_path = find_shortest_path(open_path_list)
+        print("current_path %s" % current_path)
+        visited_node_list.append(current_path[-1])
+        # print("visited_node_list %s" % visited_node_list)
+        available = graph[current_path[-1]]
+        open_path_list.remove(current_path)
+        # print("open_path_list2 %s" % open_path_list)
 
-            for neighbor in available:
-                if neighbor not in closed_node_list:
-                    print("available %s" % available)
-                    # print("current_path %s" % current_path)
-                    new_path = copy.deepcopy(current_path)
-                    new_path.append(neighbor)
-                    print("new_path %s" % new_path)
-                    if(neighbor == target):  # found target
-                        found_path_list.append(new_path)
-                        print("found target %s" % found_path_list)
-                    open_path_list.append(new_path)
-                    closed_node_list.append(neighbor)
-                    print("neighbor added to closed_node %s" % neighbor)
-                    # print("closed_node_list %s" % closed_node_list)
-
+        for neighbor in available:
+            if neighbor not in visited_node_list:
+                print("available1 %s" % available)
+                # print("current_path %s" % current_path)
+                new_path = copy.copy(current_path)
+                new_path.append(neighbor)
+                # print("new_path %s" % new_path)
+                if(neighbor == target):  # found target
+                    found_path_list.append(new_path)
+                    print("found target %s" % found_path_list)
+                open_path_list.append(new_path)
+                visited_node_list.append(neighbor)
+                print("neighbor added to closed_node %s" % neighbor)
+                # print("visited_node_list %s" % visited_node_list)
+        print("available2 %s" % available)
         if len(available) > 0:
             next = available[0]
             current_path.append(next)
-            closed_node_list.append(next)
+            visited_node_list.append(next)
             print("next %s" % next)
         else:
             current_path.pop()
